3e8c9622286d103f3308b700d9f15dea2f8b2c5d
fDynamo <fontaine.dynamo@gmail.com>
2024-09-29 15:34:37 -0700

    first

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <span>© 2024 FBDStudio</span>
+    </footer>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import { render } from "preact";
+import { LocationProvider, Router, Route } from "preact-iso";
+
+import Header from "./components/Header";
+import { Home } from "./pages/Home/index.jsx";
+import { NotFound } from "./pages/_404.jsx";
+import "./style.css";
+import Footer from "./components/Footer";
+
+export function App() {
+  return (
+    <LocationProvider>
+      <Header />
+      <main>
+        <Router>
+          <Route path="/" component={Home} />
+          <Route default component={NotFound} />
+        </Router>
+      </main>
+      <Footer />
+    </LocationProvider>
+  );
+}
+
+render(<App />, document.getElementById("app"));
