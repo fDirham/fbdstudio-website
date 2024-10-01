b0a66362452cbce58e67c0c25d2073bae6b090bf
fDynamo <fontaine.dynamo@gmail.com>
2024-10-01 16:54:26 -0700

    mode transitions

--- src/pages/Home/HomeView.tsx
+++ src/pages/Home/HomeView.tsx
@@ -4,6 +4,8 @@ import Footer from "../../components/Footer";
 import AppCarousel from "../../components/AppCarousel";
 import DetailMode from "../../components/DetailMode";
 import { AppInfo, UseStateValue } from "../../utilities/customTypes";
+import { useEffect, useState } from "preact/hooks";
+import { useAnimate } from "framer-motion";
 
 type HomeViewProps = {
   isDetailModeState: UseStateValue<boolean>;
@@ -24,6 +26,53 @@ export default function HomeView(props: HomeViewProps) {
     onNextChosenAppIndex,
   } = props;
   const [isDetailMode, setIsDetailMode] = isDetailModeState;
+  const [dmScope, dmAnimate] = useAnimate();
+  const [canAnimate, setCanAnimate] = useState(false);
+
+  useEffect(() => {
+    let animDuration = 0.4;
+    if (!canAnimate) {
+      animDuration = 0;
+    }
+
+    if (isDetailMode) {
+      dmAnimate(
+        "div.homeMode",
+        { opacity: 0 },
+        {
+          duration: animDuration,
+          onComplete: () => {
+            dmAnimate("div.homeMode", { display: "none" }, { duration: 0 });
+            dmAnimate(
+              "div.detailMode",
+              { opacity: 1, display: "initial" },
+              { duration: animDuration }
+            );
+          },
+        }
+      );
+    } else {
+      dmAnimate(
+        "div.detailMode",
+        { opacity: 0 },
+        {
+          duration: animDuration,
+          onComplete: () => {
+            dmAnimate("div.detailMode", { display: "none" }, { duration: 0 });
+            dmAnimate(
+              "div.homeMode",
+              { opacity: 1, display: "initial" },
+              { duration: animDuration }
+            );
+          },
+        }
+      );
+    }
+  }, [isDetailMode]);
+
+  useEffect(() => {
+    setCanAnimate(true);
+  }, []);
 
   function toggleDetailMode() {
     setIsDetailMode((curr) => !curr);
@@ -35,8 +84,8 @@ export default function HomeView(props: HomeViewProps) {
         onShowDetailToggle={toggleDetailMode}
         isDetailMode={isDetailMode}
       />
-      <main>
-        {isDetailMode ? (
+      <main ref={dmScope}>
+        <div class="detailMode" style={{ display: "none" }}>
           <DetailMode
             appInfoArr={appInfoArr}
             chosenAppIndexState={chosenAppIndexState}
@@ -44,22 +93,20 @@ export default function HomeView(props: HomeViewProps) {
             onBackChosenAppIndex={onBackChosenAppIndex}
             onNextChosenAppIndex={onNextChosenAppIndex}
           />
-        ) : (
-          <>
-            <div class={styles.heroTextContainer}>
-              <h1>We make apps!</h1>
-              <h2>-FBDStudio</h2>
-            </div>
-            <AppCarousel
-              appInfoArr={appInfoArr}
-              chosenAppIndexState={chosenAppIndexState}
-              isScrollDirectionRight={isScrollDirectionRight}
-              onBackChosenAppIndex={onBackChosenAppIndex}
-              onNextChosenAppIndex={onNextChosenAppIndex}
-            />
-          </>
-        )}
-
+        </div>
+        <div class="homeMode" style={{ display: "none" }}>
+          <div class={styles.heroTextContainer}>
+            <h1>We make apps!</h1>
+            <h2>-FBDStudio</h2>
+          </div>
+          <AppCarousel
+            appInfoArr={appInfoArr}
+            chosenAppIndexState={chosenAppIndexState}
+            isScrollDirectionRight={isScrollDirectionRight}
+            onBackChosenAppIndex={onBackChosenAppIndex}
+            onNextChosenAppIndex={onNextChosenAppIndex}
+          />
+        </div>
         <div class={styles.showDetail__container}>
           <button
             class={styles.showDetail__button}
