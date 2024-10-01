7b11026036ca016f012e51ea39a31ce8cca60c9d
fDynamo <fontaine.dynamo@gmail.com>
2024-10-01 11:03:06 -0700

    home view and container setup

---
+++ src/pages/Home/HomeContainer.tsx
@@ -0,0 +1,5 @@
+import HomeView from "./HomeView";
+
+export default function HomeContainer() {
+  return <HomeView />;
+}
---
+++ src/pages/Home/HomeView.tsx
@@ -0,0 +1,50 @@
+import { useState } from "preact/hooks";
+import styles from "./HomeView.module.css";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import AppCarousel from "../../components/AppCarousel";
+import DetailMode from "../../components/DetailMode";
+import { appInfoArr } from "../../constants";
+
+export default function HomeView() {
+  const [isDetailMode, setIsDetailMode] = useState(true);
+  const [chosenAppIndex, setChosenAppIndex] = useState(0);
+
+  function toggleDetailMode() {
+    setIsDetailMode((curr) => !curr);
+  }
+
+  return (
+    <div class={styles.pageContainer}>
+      <Header />
+      <main>
+        <div class={styles.heroTextContainer}>
+          <h1>We make apps!</h1>
+          <h2>-FBDStudio</h2>
+        </div>
+        {isDetailMode ? (
+          <DetailMode
+            appInfoArr={appInfoArr}
+            chosenAppIndexState={[chosenAppIndex, setChosenAppIndex]}
+          />
+        ) : (
+          <AppCarousel
+            appInfoArr={appInfoArr}
+            chosenAppIndexState={[chosenAppIndex, setChosenAppIndex]}
+          />
+        )}
+
+        <div class={styles.showDetail__container}>
+          <button
+            class={styles.showDetail__button}
+            onClick={toggleDetailMode}
+            disabled={false}
+          >
+            {isDetailMode ? "Hide detail" : "Show detail"}
+          </button>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
